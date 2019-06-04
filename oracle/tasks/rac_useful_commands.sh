8e25fd9d34e0121d121b11402a128aa4cd122a9d
dataniard <dataniard@gmail.com>
2019-06-05 00:56:22 +0200

    2019-06-05

--- oracle/tasks/rac_useful_commands.sh
+++ oracle/tasks/rac_useful_commands.sh
@@ -1,4 +1,4 @@
--- List of databases
+## List of databases
 srvctl config database
--- New oratab, list databases with ORACLE_HOME
+## New oratab, list databases with ORACLE_HOME
 srvctl config database -v
